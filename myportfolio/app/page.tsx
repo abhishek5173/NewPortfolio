298207ac1ab4aa2df6b5e255834236967d76e79b
Abhishek <114069034+abhishek5173@users.noreply.github.com>
2024-12-26 21:04:35 +0530

    added new resume

--- myportfolio/app/page.tsx
+++ myportfolio/app/page.tsx
@@ -53,7 +53,7 @@ export default function Home() {
          <a
             target="blank"
             className="font-extrabold px-2 py-2 text-white backdrop-blur-sm border border-white rounded-full hover:shadow-[0px_0px_4px_4px_rgba(0,0,0,0.1)] text-sm transition duration-200"
-            href="https://drive.google.com/file/d/12QwbMAnv6KR4m8z-Nsaw5nbt-1VfNKL8/view?usp=sharing"
+            href="https://drive.google.com/file/d/16IIFv4ek4bGSWSMKlxa_mjSSi1vOwVhj/view?usp=drive_link"
           >
             <Cover className="text-white text-lg">&lt;View Resume&#47;&gt;</Cover>
           </a>
